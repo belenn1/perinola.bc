c5aa8cac8bba0b517fa177940584857b296131a8
Belen Chijo <belennnc25026@gmail.com>
2024-08-28 11:03:11 -0300

    hice el archivo perinola y el principal para probarlo

---
+++ perinola.py
@@ -0,0 +1,9 @@
+class Perinola:
+ def __init__(self):
+  self.cara_visible = 1
+ def __repr__(self):
+  return f"Salio: {self.cara_visible}"
+ def tirar(self):
+  self.cara_visible = choice(1,self.caras)
+  return self.cara_visible
+  
